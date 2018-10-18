5a01e1bf633728a86064aa04bfec97fc96600798
HuangRong <n008385@archermind.com>
2018-10-18 19:51:23 +0800

    add BaiduSDK

---
+++ app/src/main/java/com/baidu/iov/dueros/waimai/ui/WaiMaiApplication.java
@@ -0,0 +1,19 @@
+package com.baidu.iov.dueros.waimai.ui;
+
+import android.app.Application;
+
+import com.baidu.iov.dueros.waimai.utils.LocationUtils;
+import com.baidu.mapapi.SDKInitializer;
+
+public class WaiMaiApplication extends Application {
+    public static LocationUtils mLocationUtils = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SDKInitializer.initialize(this);
+        mLocationUtils = LocationUtils.getInstance(getApplicationContext());
+        mLocationUtils.getLcation(null, null, 2000, true);
+        mLocationUtils.startLocation();
+    }
+}
