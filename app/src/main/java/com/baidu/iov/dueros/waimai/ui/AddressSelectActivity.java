cc64e8e138e4c22f0acdcc5ef195d9fbedab4521
XuShengsheng <n010959@archermind.com>
2018-11-12 14:42:50 +0800

    添加百度导航地址
    Change-Id: I27252bc4ea686e58579341d20fe5e8723b11fb6d

--- app/src/main/java/com/baidu/iov/dueros/waimai/ui/AddressSelectActivity.java
+++ app/src/main/java/com/baidu/iov/dueros/waimai/ui/AddressSelectActivity.java
@@ -1,6 +1,10 @@
 package com.baidu.iov.dueros.waimai.ui;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,9 +14,13 @@ import android.view.View;
 import com.baidu.iov.dueros.waimai.R;
 import com.baidu.iov.dueros.waimai.adapter.AddressSelectAdapter;
 import com.baidu.iov.dueros.waimai.net.entity.request.AddressListReqBean;
+import com.baidu.iov.dueros.waimai.net.entity.response.AddressEndBean;
 import com.baidu.iov.dueros.waimai.net.entity.response.AddressListBean;
 import com.baidu.iov.dueros.waimai.presenter.AddressSelectPresenter;
 import com.baidu.iov.dueros.waimai.utils.Constant;
+import com.baidu.iov.dueros.waimai.utils.Lg;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +30,7 @@ public class AddressSelectActivity extends BaseActivity<AddressSelectPresenter, 
     private AddressListBean.IovBean.DataBean mHeaderDataBean;
     private RecyclerView mRecyclerView;
     private AddressSelectAdapter mAdapter;
+    private AddressEndBean navi;
 
     @Override
     AddressSelectPresenter createPresenter() {
@@ -37,10 +46,41 @@ public class AddressSelectActivity extends BaseActivity<AddressSelectPresenter, 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_select);
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.baidu.naviauto.open.api");
+        MReceiver mReceiver = new MReceiver();
+        registerReceiver(mReceiver, intentFilter);
+        Intent i = new Intent();
+        i.setData(Uri.parse("bdmapauto://map/navi_end_route"));
+        startActivity(i);
         mDataList = new ArrayList<>();
         getPresenter().requestData(new AddressListReqBean());
         initView();
         initData();
+    }
+
+    class MReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction() == "com.baidu.naviauto.open.api"){
+                String s = intent.getStringExtra("dest_json");
+                Lg.getInstance().d("xss","getAction   "+s);
+                parseJSON(s);
+                Lg.getInstance().d("xss","AddOnSuccess");
+                AddressListBean.IovBean.DataBean dataBean = new AddressListBean.IovBean.DataBean();
+                Lg.getInstance().d("xss","AddOnSuccess =  "+navi.getAddress());
+                dataBean.setUser_name("xss");
+                dataBean.setUser_phone("1888888888");
+                dataBean.setAddress(navi.getAddress()+navi.getName());
+                mDataList.add(0,dataBean);
+            }
+        }
+    }
+
+    private void parseJSON(String jsonData) {
+        Gson gson = new Gson();
+        navi = gson.fromJson(jsonData,
+                new TypeToken<AddressEndBean>() {}.getType());
     }
 
     private void initData() {
@@ -60,8 +100,8 @@ public class AddressSelectActivity extends BaseActivity<AddressSelectPresenter, 
             public void OnItemClick(View v, AddressListBean.IovBean.DataBean dataBean) {
                 switch (v.getId()) {
                     case R.id.address_select_des:
-                    case R.id.address_select_details_container:
-                        finish();
+//                    case R.id.address_select_details_container:
+//                        finish();
                         break;
                     case R.id.address_select_edit:
                         Intent intent = new Intent(AddressSelectActivity.this, AddressEditActivity.class);
