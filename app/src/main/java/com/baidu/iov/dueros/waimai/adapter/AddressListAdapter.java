9cfa4923de6013c2390522bae73c5a3534158083
zhangbing <bing.zhang@archermind.com>
2018-11-15 11:43:03 +0800

    modify address layout
    Change-Id: Ib01156eca88fefb941cf7b46dcbb5a24ef33bf12

--- app/src/main/java/com/baidu/iov/dueros/waimai/adapter/AddressListAdapter.java
+++ app/src/main/java/com/baidu/iov/dueros/waimai/adapter/AddressListAdapter.java
@@ -1,6 +1,8 @@
 package com.baidu.iov.dueros.waimai.adapter;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,10 +16,11 @@ import com.baidu.iov.dueros.waimai.utils.Encryption;
 
 import java.util.List;
 
-public class AddressListAdapter extends BaseAdapter {
+public class AddressListAdapter extends RecyclerView.Adapter<AddressListAdapter.ViewHolder> implements View.OnClickListener {
 
     private Context mContext;
     private List<AddressListBean.IovBean.DataBean> mData;
+    private OnItemClickListener mOnItemClickListener;
 
     public AddressListAdapter(Context mContext) {
         this.mContext = mContext;
@@ -28,68 +31,100 @@ public class AddressListAdapter extends BaseAdapter {
         notifyDataSetChanged();
     }
 
-    @Override
-    public int getCount() {
-        return mData == null ? 0 : mData.size();
-    }
 
-    @Override
-    public Object getItem(int position) {
-        return mData == null ? null : mData.get(position);
-    }
+    class ViewHolder extends RecyclerView.ViewHolder {
 
-    @Override
-    public long getItemId(int position) {
-        return mData == null ? 0 : position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        ViewHolder viewHolder;
-        if (convertView == null) {
-            convertView = View.inflate(mContext, R.layout.list_address_item, null);
-            viewHolder = new ViewHolder();
-            viewHolder.img_select = convertView.findViewById(R.id.img_select);
-            viewHolder.tv_address = convertView.findViewById(R.id.tv_address);
-            viewHolder.tv_address_type = convertView.findViewById(R.id.tv_address_type);
-            viewHolder.tv_phone = convertView.findViewById(R.id.tv_phone);
-            viewHolder.tv_name = convertView.findViewById(R.id.tv_name);
-            viewHolder.img_add = convertView.findViewById(R.id.img_add);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (ViewHolder) convertView.getTag();
-        }
-
-        try {
-            String address = Encryption.desEncrypt(mData.get(position).getAddress());
-            viewHolder.tv_address.setText(address);
-
-            //String address_type = mData.get(position).getAddressRangeTip();
-            //viewHolder.tv_address_type.setText(address_type);
-
-            String name = Encryption.desEncrypt(mData.get(position).getUser_name());
-            viewHolder.tv_name.setText(name);
-
-            String phone = Encryption.desEncrypt(mData.get(position).getUser_phone());
-            viewHolder.tv_phone.setText(phone);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        return convertView;
-    }
-
-
-    public class ViewHolder {
-
-        ImageView img_select;
+        TextView tv_serial;
         TextView tv_address;
         TextView tv_address_type;
         TextView tv_name;
         TextView tv_phone;
-        CheckBox img_add;
+        ImageView img_edit;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tv_serial = itemView.findViewById(R.id.tv_num);
+            tv_address = itemView.findViewById(R.id.tv_address);
+            tv_address_type = itemView.findViewById(R.id.tv_address_type);
+            tv_name = itemView.findViewById(R.id.tv_name);
+            tv_phone = itemView.findViewById(R.id.tv_phone);
+            img_edit = itemView.findViewById(R.id.img_select);
+
+            itemView.setOnClickListener(AddressListAdapter.this);
+            img_edit.setOnClickListener(AddressListAdapter.this);
+        }
+    }
+
+    @Override
+    public AddressListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View convertView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_address_item, parent, false);
+        return new ViewHolder(convertView);
+    }
+
+    @Override
+    public void onBindViewHolder(AddressListAdapter.ViewHolder holder, int position) {
+
+        try {
+            holder.tv_serial.setText(String.valueOf((position + 1)));
+
+            String address = Encryption.desEncrypt(mData.get(position).getAddress());
+            holder.tv_address.setText(address);
+
+            String address_type = mData.get(position).getType();
+            if (address_type != null) {
+
+                holder.tv_address_type.setText(address_type);
+            } else {
+                holder.tv_address_type.setText(mContext.getString(R.string.address_tag_other));
+            }
+
+            String name = Encryption.desEncrypt(mData.get(position).getUser_name());
+            holder.tv_name.setText(name);
+
+            String phone = Encryption.desEncrypt(mData.get(position).getUser_phone());
+            holder.tv_phone.setText(phone);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        holder.itemView.setTag(position);
+        holder.img_edit.setTag(position);
 
     }
+
+    @Override
+    public int getItemCount() {
+        return mData == null ? 0 : mData.size();
+    }
+
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+
+    public void setOnItemClickListener(OnItemClickListener itemClickListener) {
+        this.mOnItemClickListener = itemClickListener;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        int position = (int) v.getTag();
+        if (mOnItemClickListener != null) {
+            switch (v.getId()) {
+                case R.id.img_select:
+                    mOnItemClickListener.onItemClick(v, position);
+                    break;
+                default:
+                    mOnItemClickListener.onItemClick(v, position);
+                    break;
+            }
+        }
+    }
+
 }
+
