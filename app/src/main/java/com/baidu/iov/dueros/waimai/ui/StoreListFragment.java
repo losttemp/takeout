589cb04a6a640c87a5b507e1fca118c67d73e9bb
ping20181008 <1029980215@qq.com>
2018-12-06 11:21:03 +0800

    fix
    Change-Id: Id6e0913aca62ac19ef8634b0dcd8c62910f61bee

--- app/src/main/java/com/baidu/iov/dueros/waimai/ui/StoreListFragment.java
+++ app/src/main/java/com/baidu/iov/dueros/waimai/ui/StoreListFragment.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.widget.AppCompatImageView;
 import android.support.v7.widget.AppCompatTextView;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,7 +20,6 @@ import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-
 import com.baidu.iov.dueros.waimai.R;
 import com.baidu.iov.dueros.waimai.adapter.StoreAdaper;
 import com.baidu.iov.dueros.waimai.net.entity.request.FilterConditionReq;
@@ -89,8 +87,7 @@ public class StoreListFragment extends BaseFragment<StoreListPresenter, StoreLis
 	private FilterConditionReq filterConditionReq;
 
 	private LinearLayout mLoading;
-
-	private LocalBroadcastManager broadcastManager;
+	
 
 
 
