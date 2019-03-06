5fe287586fe029b4cf6cd3156d45fd58c7488f16
dinglai <lai.ding@archermind.com>
2019-03-06 17:45:56 +0800

    城市界面UI优化
    Change-Id: I8a1e8e8e3ed715c3e57a61e80da72cf60b092c91

--- app/src/main/java/com/baidu/iov/dueros/waimai/adapter/CYBChangeCityGridViewAdapter.java
+++ app/src/main/java/com/baidu/iov/dueros/waimai/adapter/CYBChangeCityGridViewAdapter.java
@@ -5,10 +5,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.baidu.iov.dueros.waimai.R;
 import com.baidu.iov.dueros.waimai.net.entity.response.CityListBean;
-import com.domain.multipltextview.MultiplTextView;
 
 import java.util.List;
 
@@ -17,49 +17,50 @@ import java.util.List;
  */
 public class CYBChangeCityGridViewAdapter extends BaseAdapter {
 
-	private List<CityListBean.HotBean> list;
-	private Context context;
-	private LayoutInflater inflater;
+    private List<CityListBean.HotBean> list;
+    private Context context;
+    private LayoutInflater inflater;
 
-	public CYBChangeCityGridViewAdapter(Context context, List<CityListBean.HotBean> list){
-		this.context=context;
-		this.list=list;
-		inflater= LayoutInflater.from(context);
-	}
+    public CYBChangeCityGridViewAdapter(Context context, List<CityListBean.HotBean> list) {
+        this.context = context;
+        this.list = list;
+        inflater = LayoutInflater.from(context);
+    }
 
-	@Override
+    @Override
     public int getCount() {
-		return list.size();
-	}
+        return list.size();
+    }
 
-	@Override
+    @Override
     public Object getItem(int position) {
-		return list.get(position);
-	}
+        return list.get(position);
+    }
 
-	@Override
+    @Override
     public long getItemId(int position) {
-		return position;
-	}
+        return position;
+    }
 
-	@Override
+    @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-		ViewHolder viewHolder=null;
-		if(convertView==null){
-			convertView=inflater.inflate(R.layout.item_gridview_cyb_change_city,null);
-			viewHolder=new ViewHolder(convertView);
-			convertView.setTag(viewHolder);
-		}else {
-			viewHolder=(ViewHolder)convertView.getTag();
-		}
-		viewHolder.city_hot.setText(list.get(position).getName());
-		return convertView;
-	}
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_gridview_cyb_change_city, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.city_hot.setText(list.get(position).getName());
+        return convertView;
+    }
 
-	class ViewHolder{
-		private MultiplTextView city_hot;
-		public ViewHolder(View view){
-			city_hot=(MultiplTextView)view.findViewById(R.id.city_hot);
-		}
-	}
+    class ViewHolder {
+        private TextView city_hot;
+
+        public ViewHolder(View view) {
+            city_hot = view.findViewById(R.id.city_hot);
+        }
+    }
 }
